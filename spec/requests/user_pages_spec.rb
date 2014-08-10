4af95dd74378b019d9839e985ee3198597cb0051
Dan Wrona <djwrona@comcast.net>
2014-08-10 15:46:48 -0600

    Finish user signup 2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,6 +21,36 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "bad username, email, & password" do
+        before do
+	  fill_in "Name",         with: " "
+	  fill_in "Email",        with: "user@example..com"
+	  fill_in "Password",     with: "fooba"
+	  fill_in "Confirmation", with: "fooba"
+          click_button submit
+        end
+
+        it { should have_title('Sign up') }
+        it { should have_content('The form contains 3 errors.') }
+        it { should have_content('Name can\'t be blank') }
+        it { should have_content('Email is invalid') }
+        it { should have_content('Password is too short (minimum is 6 characters)') }
+      end
+
+      describe "mismatching passwords" do
+        before do
+	  fill_in "Name",         with: "Dan Wrona "
+	  fill_in "Email",        with: "djw@1.com"
+	  fill_in "Password",     with: "passwordOne"
+	  fill_in "Confirmation", with: "passwordTwo"
+          click_button submit
+        end
+
+        it { should have_title('Sign up') }
+        it { should have_content('The form contains 1 error.') }
+        it { should have_content('Password confirmation doesn\'t match Password') }
+      end
     end
 
     describe "with valid information" do
@@ -33,6 +63,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
