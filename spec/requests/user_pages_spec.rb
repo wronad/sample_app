e6c2feb95ae86cb077016766a3fc4d14f20a8078
Dan Wrona <djwrona@comcast.net>
2014-08-23 11:38:04 -0600

    update show users only

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,20 +2,46 @@ require 'spec_helper'
 
 describe "User pages" do
 
+  let(:submit) { "Create my account" }
+
   subject { page }
+
+  describe "index" do
+    let(:user) { FactoryGirl.create(:user) }
+    before(:each) do
+      sign_in user
+      visit users_path
+    end
+
+    it { should have_title('All users') }
+    it { should have_content('All users') }
+
+    describe "pagination" do
+
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
+    end
+  end
 
   describe "signup page" do
     before { visit signup_path }
 
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
+    it { should have_submit_button(submit) }
   end
 
   describe "signup" do
 
     before { visit signup_path }
-
-    let(:submit) { "Create my account" }
 
     describe "with invalid information" do
       it "should not create a user" do
@@ -45,16 +71,15 @@ describe "User pages" do
       before { submit_signup('Example User', 'user@example.com', 'foobar', 'foobar') }
 
       it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+        expect { change(User, :count).by(1) }
       end
 
       describe "after saving the user" do
-        before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome') }
       end
     end
   end
@@ -67,4 +92,35 @@ describe "User pages" do
     it { should have_title(user.name) }
   end
 
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before { save_user(new_name, new_email, user.password, user.password) }
+
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+    end
+  end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,11 +6,40 @@ def valid_signin(user)
   click_button "Sign in"
 end
 
-def submit_signup(name, email, passwd, passwd1)
+def fill_in_user(name, email, passwd, passwd1)
   fill_in "Name",         with: name
   fill_in "Email",        with: email
   fill_in "Password",     with: passwd
+end
+
+def submit_signup(name, email, passwd, passwd1)
+  fill_in_user(name, email, passwd, passwd1)
   fill_in "Confirmation", with: passwd1
   click_button submit
 end
 
+def save_user(name, email, passwd, passwd1)
+  fill_in_user(name, email, passwd, passwd1)
+  fill_in "Confirm Password", with: passwd1
+  click_button "Save changes"
+end
+
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.digest(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
+
+RSpec::Matchers.define :have_submit_button do |value|
+  match do |actual|
+    actual.should have_selector("input[type=submit][value='#{value}']")
+  end
+end
